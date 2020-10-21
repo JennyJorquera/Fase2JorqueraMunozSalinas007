52d8d4537196d893aff2e336a46ecd7e9c5de532
Jenny Jorquera <jen.jorquera@alumnos.duoc.cl>
2020-10-21 18:59:35 +0200

    DJJeventos: Creando Modelos

--- DJJeventos/eventos/models.py
+++ DJJeventos/eventos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+import uuid # Required for unique book instances
 
-# Create your models here.
+	
+
+class reserva(models.Model):
+	"""Model representing an reserva."""
+	codigo_reserva = models.CharField(max_length=10)
+	fecha_evento = models.DateField(null=True, blank=True)
+	nom_cliente = models.CharField(max_length=200)
+	apell_cliente = models.CharField(max_length=200)
+	celu_cliente = models.CharField(max_length=10)
+	email = models.CharField(max_length=200)
+	cant_personas = models.IntegerField(default=0)
+	total = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.codigo_reserva
