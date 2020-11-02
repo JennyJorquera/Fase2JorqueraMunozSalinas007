19ea4afb0970fbc7b7fa408e6c841332d4d459e1
Jenny Jorquera <jen.jorquera@alumnos.duoc.cl>
2020-11-01 23:12:21 -0300

    DJJeventos: Creando Pagina de Inicio

--- DJJeventos/eventos/urls.py
+++ DJJeventos/eventos/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('cotizacion/', views.cotizacion, name='cotizacion'),
+    path('descEventos/', views.descEventos, name='descEventos'),
+    path('reservas/', views.reservas, name='reservas')
 ]
+
+urlpatterns += [
+    path('reserva/create/', views.reservaCreate.as_view(), name='reserva_create'),
+    path('reserva/<int:pk>/update/', views.reservaUpdate.as_view(), name='reserva_update'),
+    path('reserva/<int:pk>/delete/', views.reservaDelete.as_view(), name='reserva_delete'),
+]
+
+    
--- DJJeventos/eventos/views.py
+++ DJJeventos/eventos/views.py
@@ -1,3 +1,74 @@
 from django.shortcuts import render
+from . models import reserva
+from django.views import generic
+
+#importamos información para formularios
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+def index(request):
+
+    num_reserva = reserva.objects.all().count()
+   
+    return render(
+        request,
+        'index.html',
+        context={'num_reserva': num_reserva},
+    )
+
+def eventos(request):
+   
+    return render(
+        request,
+        'eventos.html',
+    )
+
+def cotizacion(request):
+   
+    return render(
+        request,
+        'cotizacion.html',
+      
+    )
+
+def descEventos(request):
+   
+    return render(
+        request,
+        'descEventos.html',
+      
+    )
+
+def reservas(request):
+    
+    return render(
+        request,
+        'reservas.html',
+      
+    )
+
+class reservaCreate(CreateView):
+    model = reserva
+    fields = '__all__'
+
+class reservaUpdate(UpdateView):
+    model = reserva
+    fields = ['codigo_reserva', 'fecha_evento', 'nom_cliente', 'apell_cliente',
+                'celu_cliente', 'email', 'cant_personas', 'total']
+
+class reservaDelete(DeleteView):
+    model = reserva
+    sucess_url = reverse_lazy('reservas')
+
+
+class reservaDetailView(generic.DetailView):
+    model=reserva
+    
+    
+
+
+
+
+
